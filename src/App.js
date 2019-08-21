951e61b20d376ed2921f93299deebf503c7021f3
Terry Berlin <terryb@quikserve.com>
2019-08-21 07:54:51 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,105 +1,93 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import http from "./services/httpService";
-import config from "./config.json";
+import Groups from "./components/groups";
+import Orders from "./components/orders";
+import PrepItems from "./components/prepItems";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    posts: []
-  };
-
-  async componentDidMount() {
-    //pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await http.get(config.apiEndpoint); //when using await decorate function with async
-    this.setState({ posts });
-
-    //console.log(promise);
-    //const response = await promise; //when using await decorate function with async
-  }
-
-  handleAdd = async () => {
-    //console.log("Add");
-    const obj = { title: "a", body: "b" };
-    const { data: post } = await http.post(config.apiEndpoint, obj); //when using await decorate function with async
-    //console.log(post);
-
-    const posts = [post, ...this.state.posts];
-    this.setState({ posts });
-  };
-
-  handleUpdate = async post => {
-    post.title = "UPDATED";
-    //const { data } = await http.put(config.apiEndpoint + "/" + post.id, post);
-    //http.patch(config.apiEndpoint + "/" + post.id, {title: post.title});
-    //console.log(data);
-
-    await http.put(config.apiEndpoint + "/" + post.id, post);
-    const posts = [...this.state.posts];
-    const index = posts.indexOf(post);
-    posts[index] = { ...post };
-    this.setState({ posts });
-  };
-
-  handleDelete = async post => {
-    const originalPosts = this.state.posts;
-
-    const posts = this.state.posts.filter(p => p.id !== post.id);
-    this.setState({ posts });
-
-    try {
-      await http.delete(config.apiEndpoint + "/" + post.id);
-      //throw new Error("");
-    } catch (ex) {
-      if (ex.response && ex.response.status === 404)
-        alert("This post has already been deleted.");
-      this.setState({ posts: originalPosts });
-    }
-  };
-
   render() {
+    //state = {};
+
     return (
       <React.Fragment>
         <ToastContainer />
-        <button className="btn btn-primary" onClick={this.handleAdd}>
-          Add
-        </button>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map(post => (
-              <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>
-                  <button
-                    className="btn btn-info btn-sm"
-                    onClick={() => this.handleUpdate(post)}
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => this.handleDelete(post)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/groups" component={Groups} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/prepitems" component={PrepItems} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
     );
+
+    // handleAdd = async () => {
+    //   const obj = { title: "a", body: "b" };
+    //   const { data: post } = await http.post(config.apiEndpoint, obj); //when using await decorate function with async
+    //   const posts = [post, ...this.state.posts];
+    //   this.setState({ posts });
+    // };
+
+    // handleUpdate = async post => {
+    //   post.title = "UPDATED";
+    //   await http.put(config.apiEndpoint + "/" + post.id, post);
+    //   const posts = [...this.state.posts];
+    //   const index = posts.indexOf(post);
+    //   posts[index] = { ...post };
+    //   this.setState({ posts });
+    // };
+
+    // handleDelete = async post => {
+    //   const originalPosts = this.state.posts;
+
+    //   const posts = this.state.posts.filter(p => p.id !== post.id);
+    //   this.setState({ posts });
+
+    //   try {
+    //     await http.delete(config.apiEndpoint + "/" + post.id);
+    //     //throw new Error("");
+    //   } catch (ex) {
+    //     if (ex.response && ex.response.status === 404)
+    //       alert("This post has already been deleted.");
+    //     this.setState({ posts: originalPosts });
+    //   }
+    // };
+
+    // render() {
+    //   return (
+    //     <React.Fragment>
+    //       <ToastContainer />
+    //       <table className="table">
+    //         <thead>
+    //           <tr>
+    //             <th>Group Code</th>
+    //             <th>Group Name</th>
+    //           </tr>
+    //         </thead>
+    //         <tbody>
+    //           {this.state.posts.map(post => (
+    //             <tr key={post.group_id}>
+    //               <td>{post.group_id}</td>
+    //               <td>{post.group_name}</td>
+    //             </tr>
+    //           ))}
+    //         </tbody>
+    //       </table>
+    //     </React.Fragment>
+    //   );
+    // }
   }
 }
-
 export default App;
